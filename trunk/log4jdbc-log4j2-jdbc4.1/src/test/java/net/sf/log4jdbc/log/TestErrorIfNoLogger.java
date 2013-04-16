094718b7bc42c66ac3bf8608276360c77710fe8c
matsepp@gmail.com <matsepp@gmail.com@b4669117-0fdc-421f-5fbe-f1bdb88fe090>
2013-04-16 15:28:06 +0000

    Add a check of the content of the error message

--- trunk/log4jdbc-log4j2-jdbc4.1/src/test/java/net/sf/log4jdbc/log/TestErrorIfNoLogger.java
+++ trunk/log4jdbc-log4j2-jdbc4.1/src/test/java/net/sf/log4jdbc/log/TestErrorIfNoLogger.java
@@ -1,5 +1,7 @@
 package net.sf.log4jdbc.log;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 /**
@@ -14,11 +16,22 @@ public class TestErrorIfNoLogger
 
     /**
      * A test which expects a <code>NoClassDefFoundError</code> to be thrown
+     * and check its message content
      */
     @Test(expected=NoClassDefFoundError.class)
     public void test()
     {
-        SpyLogFactory.getSpyLogDelegator();
+        try{
+            // try to get a logger through the creation of a SpyLogDelegator
+            SpyLogFactory.getSpyLogDelegator();
+        }catch(NoClassDefFoundError e){
+            // check the content of the error message
+            assertTrue("Wrong error message attached to the NoClassDefFoundError",
+                    e.getMessage().equals("Unable to find Log4j2 as default logging library. " +
+                    "Please provide a logging library and configure a valid spyLogDelegator name in the properties file."));
+            // re throw it to pass the test
+            throw(e);
+        }
     }
 
 }
