7043a05385cb5a53a96d7a9489d5aad7e644bfac
frederic.bastian@gmail.com <frederic.bastian@gmail.com@b4669117-0fdc-421f-5fbe-f1bdb88fe090>
2014-06-06 13:09:16 +0000

    Adding Db2RdbmsSpecifics provided by qxo(qxodream@gmail.com), see http://code.google.com/p/log4jdbc/issues/detail?id=48

---
+++ log4jdbc-log4j2-jdbc3/src/main/java/net/sf/log4jdbc/sql/rdbmsspecifics/Db2RdbmsSpecifics.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2007-2012 Arthur Blake
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.log4jdbc.sql.rdbmsspecifics;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * RDBMS specifics for the IBM DB2.
+ *
+ * @author qxo(qxodream@gmail.com)
+ */
+class Db2RdbmsSpecifics extends RdbmsSpecifics {
+
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
+			"'TIMESTAMP('''yyyy-MM-dd HH:mm:ss.SSS''')'");
+
+	Db2RdbmsSpecifics() {
+		super();
+	}
+
+	@Override
+	public String formatParameterObject(Object object) {
+		if (object instanceof Date) {
+			return DATE_FORMAT.format((Date) object);
+		} 
+		return super.formatParameterObject(object);
+	}
+}
